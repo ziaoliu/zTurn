abac1b55f65c986cff5b437bc53c712bd5211a26
Ziao <Ziao@Ziao-PC>
2015-07-09 23:53:26 -0500

    Palindrome Number

---
+++ zTurn/src_2nd_round/leetcode/Q009_PalindromeNumber.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/*
+ Determine whether an integer is a palindrome. Do this without extra space.
+
+ */
+public class Q009_PalindromeNumber {
+	public class Solution {
+		public boolean isPalindrome(int x) {
+			if (x < 0)
+				return false;
+			if (x < 10)
+				return true;
+
+			int digits = 1;
+			while (x / digits >= 10)
+				digits = digits * 10;
+
+			while (x > 0) {
+				int start = x / digits;
+				int end = x % 10;
+				if (start != end)
+					return false;
+				x = (x % digits) / 10;
+				digits = digits / 100; // two digits removed
+			}
+			return true;
+		}
+	}
+}
