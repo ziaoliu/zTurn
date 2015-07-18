47223b4ac2b2c95ca82d1379b69998e5e1043f34
ziaoliu <ziao.simon@gmail.com>
2015-07-18 02:22:38 -0500

    r2 Merge Two Sorted Lists

---
+++ zTurn/src_2nd_round/leetcode/Q021_MergeTwoSortedLists.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+/*
+ Merge two sorted linked lists and return it as a new list. 
+ The new list should be made by splicing together the nodes of the first two lists.
+ 
+ http://fisherlei.blogspot.com/2013/03/leetcode-merge-two-sorted-lists-solution.html
+ */
+public class Q021_MergeTwoSortedLists {
+	public class Solution {
+		public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+			if (l1 == null && l2 == null)
+				return l1;
+			if (null == l1 || null == l2)
+				return null == l1 ? l2 : l1;
+
+			ListNode dummy = new ListNode(-1);
+			ListNode head = dummy;
+
+			while (l1 != null || l2 != null) {
+				int one = Integer.MAX_VALUE;
+				int two = Integer.MAX_VALUE;
+
+				if (l1 != null)
+					one = l1.val;
+				if (l2 != null)
+					two = l2.val;
+
+				int temp = 0;
+				if (one > two) {
+					temp = two;
+					l2 = l2.next;
+				} else {
+					temp = one;
+					l1 = l1.next;
+				}
+
+				dummy.next = new ListNode(temp);
+				dummy = dummy.next;
+			}
+
+			return head.next;
+		}
+	}
+}
