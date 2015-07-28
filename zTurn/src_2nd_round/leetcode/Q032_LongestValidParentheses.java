4b5a0721d05dbd513e9ca15178dda22d6abbad79
ziaoliu <ziao.simon@gmail.com>
2015-07-28 16:50:46 -0500

    r2 Longest Valid Parentheses

---
+++ zTurn/src_2nd_round/leetcode/Q032_LongestValidParentheses.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/*
+ Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+ For "(()", the longest valid parentheses substring is "()", which has length = 2.
+
+ Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+ */
+public class Q032_LongestValidParentheses {
+	public class Solution {
+		public int longestValidParentheses(String s) {
+			
+		}
+	}
+}
