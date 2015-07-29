439f917940b0cfd896ad5eae11c661b6bc3ab33e
ziaoliu <ziao.simon@gmail.com>
2015-07-29 09:47:20 -0500

    add DP link

--- zTurn/src_2nd_round/leetcode/Q032_LongestValidParentheses.java
+++ zTurn/src_2nd_round/leetcode/Q032_LongestValidParentheses.java
@@ -11,6 +11,9 @@ import java.util.LinkedList;
 
  http://blog.csdn.net/worldwindjp/article/details/39460161
  http://blog.csdn.net/linhuanmars/article/details/20439613
+
+ DP
+ https://leetcodenotes.wordpress.com/2013/10/19/leetcode-longest-valid-parentheses-%E8%BF%99%E7%A7%8D%E6%8B%AC%E5%8F%B7%E7%BB%84%E5%90%88%EF%BC%8C%E6%9C%80%E9%95%BF%E7%9A%84valid%E6%8B%AC%E5%8F%B7%E7%BB%84%E5%90%88%E6%9C%89%E5%A4%9A/
  */
 public class Q032_LongestValidParentheses {
 	public static void main(String[] args) {
