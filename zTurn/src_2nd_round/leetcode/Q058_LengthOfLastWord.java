90dc971ea26556e3ff0abe19f86f7d2f362b288f
ziaoliu <ziao.simon@gmail.com>
2015-08-14 00:19:55 -0500

    r2 Length of Last Word

---
+++ zTurn/src_2nd_round/leetcode/Q058_LengthOfLastWord.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/*
+ Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+ If the last word does not exist, return 0.
+
+ Note: A word is defined as a character sequence consists of non-space characters only.
+
+ For example, 
+ Given s = "Hello World",
+ return 5.
+ */
+public class Q058_LengthOfLastWord {
+	public class Solution {
+		public int lengthOfLastWord(String s) {
+			if (s == null || s.length() == 0)
+				return 0;
+			int end = s.length() - 1;
+			while (end >= 0 && s.charAt(end) == ' ')
+				end--;
+			int start = end;
+			while (start >= 0 && s.charAt(start) != ' ')
+				start--;
+			return end - start;
+		}
+	}
+}
