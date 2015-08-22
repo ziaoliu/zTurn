424dee17af2e8c56d94c0db2e277393f009da30d
ziaoliu <ziao.simon@gmail.com>
2015-08-22 11:56:50 -0500

    set matrix zeros

---
+++ zTurn/src_1st_round/leetcode/SetMatrixZeros.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+public class SetMatrixZeros {
+	public class Solution {
+		public void setZeroes(int[][] matrix) {
+			if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+				return;
+			boolean firstRow = false;
+			boolean firstCol = false;
+			for (int i = 0; i < matrix.length; i++)
+				if (matrix[i][0] == 0) {
+					firstCol = true;
+					break;
+				}
+			for (int i = 0; i < matrix[0].length; i++)
+				if (matrix[0][i] == 0) {
+					firstRow = true;
+					break;
+				}
+			for (int i = 1; i < matrix.length; i++)
+				for (int j = 1; j < matrix[0].length; j++) {
+					if (matrix[i][j] == 0) {
+						matrix[i][0] = 0;
+						matrix[0][j] = 0;
+						// break; <====== cannot break yet
+					}
+				}
+			for (int i = 1; i < matrix.length; i++)
+				for (int j = 1; j < matrix[0].length; j++) {
+					if (matrix[i][0] == 0 || matrix[0][j] == 0)
+						matrix[i][j] = 0;
+				}
+			if (firstRow)
+				for (int i = 0; i < matrix[0].length; i++)
+					matrix[0][i] = 0;
+			if (firstCol)
+				for (int i = 0; i < matrix.length; i++)
+					matrix[i][0] = 0;
+		}
+	}
+}
